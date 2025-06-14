81af4a24c6a51ce5bb91263f05ec3ba76cd2dab1
Vanderleia <vanderleiasantosdaconceicaosan@gmail.com>
2025-06-14 23:56:38 +0000

    Completei o que faltava

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,12 +2,12 @@
 
 int main () {
   // Variáveis da Carta 1
-    char estado1[50], codigo1[50], cidade1[50];
+    char estado1[80], codigo1[80], cidade1[80];
     int populacao1, pontos1;
     float area1, pib1;
 
   // Variáveis da Carta 2
-    char estado2[50], codigo2[50], cidade2[50];
+    char estado2[80], codigo2[80], cidade2[80];
     int populacao2, pontos2;
     float area2, pib2;
 
