196eac5deeaeac58066583a9586074bfaeb961df
Vanderleia <vanderleiasantosdaconceicaosan@gmail.com>
2025-06-14 23:05:28 +0000

    Declarei a variável da seguanda carta

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main () {
-// Variáveis da Carta 1
+  // Variáveis da Carta 1
     char estado1[50], codigo1[50], cidade1[50];
     int populacao1, pontos1;
     float area1, pib1;
+
+  // Variáveis da Carta 2
+    char estado2[50], codigo2[50], cidade2[50];
+    int populacao2, pontos2;
+    float area2, pib2;
+
+
+
 
     return 0;
 }
